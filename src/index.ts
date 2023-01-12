d076e8542c84ac457c599c4c083a75bc118335ab
shadow578 <52449218+shadow578@users.noreply.github.com>
2023-01-12 16:55:56 +0100

    re-export all types and functions from `index`

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello, World!');
+// PowerShellBinding.ts
+import { IPowerShellBinding, PowerShellBinding, PowerShellBindingOptions } from './binding/PowerShellBinding';
+export { IPowerShellBinding, PowerShellBinding, PowerShellBindingOptions };
+
+// PowerShellCall.ts
+import { PowerShellCall, PowerShellCallOptions, psCall } from './binding/PowerShellCall';
+export { PowerShellCall, PowerShellCallOptions as PowerShellDecoratorOptions, psCall };
+
+// PowerShellError.ts
+import { PowerShellError } from './powershell/PowerShellError';
+export { PowerShellError };
+
+// PowerShellException.ts
+import { PowerShellException, isPowerShellException } from './powershell/PowerShellException';
+export { PowerShellException, isPowerShellException };
+
+// Shim.ts
+import { Shim, ShimOptions, ShimParseResult } from './powershell/shim/Shim';
+export { Shim, ShimOptions, ShimParseResult };
+
+// Parameters.ts
+import { ParameterValue, ParameterRecord, assertParametersValid } from './powershell/shim/Parameters';
+export { ParameterValue, ParameterRecord, assertParametersValid };
+
+// DefaultShim.ts
+import { DefaultShim } from './powershell/shim/DefaultShim';
+export { DefaultShim };
+
+/**
+ * shorthand for {@link PowerShellCall}
+ */
+export const PS = PowerShellCall;
